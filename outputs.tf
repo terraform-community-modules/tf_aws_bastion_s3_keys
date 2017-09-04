1c863b3dfd52d9860235f6f30cac629359d56a75
Robin Bowes <robin.bowes@yo61.com>
2017-09-04 13:40:30 +0100

    Access bastion host via elb

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "ssh_user" {
 output "security_group_id" {
   value = "${aws_security_group.bastion.id}"
 }
+
+output "dns_name" {
+  value = "${aws_elb.bastion.dns_name}"
+}
+
+output "zone_id" {
+  value = "${aws_elb.bastion.zone_id}"
+}
