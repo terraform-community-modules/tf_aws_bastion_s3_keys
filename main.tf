e34ee9b51cc3387e7a3341950356a8acc613f23a
Antonio Terreno <antonio.terreno@gmail.com>
2017-10-05 13:30:39 +0100

    Identation fix

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "bastion" {
-  name = "${var.name}"
-  vpc_id = "${var.vpc_id}"
+  name        = "${var.name}"
+  vpc_id      = "${var.vpc_id}"
   description = "Bastion security group (only SSH inbound access is allowed)"
 
   tags {
@@ -9,34 +9,39 @@ resource "aws_security_group" "bastion" {
 }
 
 resource "aws_security_group_rule" "ssh_ingress" {
-  type = "ingress"
-  from_port = "22"
-  to_port = "22"
-  protocol = "tcp"
-  cidr_blocks = "${var.allowed_cidr}"
-  ipv6_cidr_blocks = "${var.allowed_ipv6_cidr}"
+  type              = "ingress"
+  from_port         = "22"
+  to_port           = "22"
+  protocol          = "tcp"
+  cidr_blocks       = "${var.allowed_cidr}"
+  ipv6_cidr_blocks  = "${var.allowed_ipv6_cidr}"
   security_group_id = "${aws_security_group.bastion.id}"
 }
 
 resource "aws_security_group_rule" "ssh_sg_ingress" {
-  count = "${length(var.allowed_security_groups)}"
-  type = "ingress"
-  from_port = "22"
-  to_port = "22"
-  protocol = "tcp"
+  count                    = "${length(var.allowed_security_groups)}"
+  type                     = "ingress"
+  from_port                = "22"
+  to_port                  = "22"
+  protocol                 = "tcp"
   source_security_group_id = "${element(var.allowed_security_groups, count.index)}"
-  security_group_id = "${aws_security_group.bastion.id}"
+  security_group_id        = "${aws_security_group.bastion.id}"
 }
 
 resource "aws_security_group_rule" "bastion_all_egress" {
-  type = "egress"
+  type      = "egress"
   from_port = "0"
-  to_port = "65535"
-  protocol = "all"
+  to_port   = "65535"
+  protocol  = "all"
+
   cidr_blocks = [
-    "0.0.0.0/0"]
+    "0.0.0.0/0",
+  ]
+
   ipv6_cidr_blocks = [
-    "::/0"]
+    "::/0",
+  ]
+
   security_group_id = "${aws_security_group.bastion.id}"
 }
 
@@ -44,11 +49,11 @@ data "template_file" "user_data" {
   template = "${file("${path.module}/${var.user_data_file}")}"
 
   vars {
-    s3_bucket_name = "${var.s3_bucket_name}"
-    s3_bucket_uri = "${var.s3_bucket_uri}"
-    ssh_user = "${var.ssh_user}"
-    keys_update_frequency = "${var.keys_update_frequency}"
-    enable_hourly_cron_updates = "${var.enable_hourly_cron_updates}"
+    s3_bucket_name              = "${var.s3_bucket_name}"
+    s3_bucket_uri               = "${var.s3_bucket_uri}"
+    ssh_user                    = "${var.ssh_user}"
+    keys_update_frequency       = "${var.keys_update_frequency}"
+    enable_hourly_cron_updates  = "${var.enable_hourly_cron_updates}"
     additional_user_data_script = "${var.additional_user_data_script}"
   }
 }
@@ -69,19 +74,19 @@ data "template_file" "user_data" {
 //}
 
 resource "aws_launch_configuration" "bastion" {
-  name_prefix = "${var.name}-"
-  image_id = "${var.ami}"
-  instance_type = "${var.instance_type}"
-  user_data = "${data.template_file.user_data.rendered}"
+  name_prefix       = "${var.name}-"
+  image_id          = "${var.ami}"
+  instance_type     = "${var.instance_type}"
+  user_data         = "${data.template_file.user_data.rendered}"
   enable_monitoring = "${var.enable_monitoring}"
 
   security_groups = [
     "${compact(concat(list(aws_security_group.bastion.id), split(",", "${var.security_group_ids}")))}",
   ]
 
-  iam_instance_profile = "${var.iam_instance_profile}"
+  iam_instance_profile        = "${var.iam_instance_profile}"
   associate_public_ip_address = "${var.associate_public_ip_address}"
-  key_name = "${var.key_name}"
+  key_name                    = "${var.key_name}"
 
   lifecycle {
     create_before_destroy = true
@@ -95,14 +100,14 @@ resource "aws_autoscaling_group" "bastion" {
     "${var.subnet_ids}",
   ]
 
-  desired_capacity = "1"
-  min_size = "1"
-  max_size = "1"
+  desired_capacity          = "1"
+  min_size                  = "1"
+  max_size                  = "1"
   health_check_grace_period = "60"
-  health_check_type = "EC2"
-  force_delete = false
+  health_check_type         = "EC2"
+  force_delete              = false
   wait_for_capacity_timeout = 0
-  launch_configuration = "${aws_launch_configuration.bastion.name}"
+  launch_configuration      = "${aws_launch_configuration.bastion.name}"
 
   enabled_metrics = [
     "GroupMinSize",
@@ -122,7 +127,8 @@ resource "aws_autoscaling_group" "bastion" {
           map("key", "EIP", "value", "${var.eip}", "propagate_at_launch", true)
         ),
         var.extra_tags)
-      }"]
+      }",
+  ]
 
   lifecycle {
     create_before_destroy = true
