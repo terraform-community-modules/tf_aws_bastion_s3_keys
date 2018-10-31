2f7b1be589d268a6ab1e989ea3ec4854e02f0d27
Charles-Henri GUERIN <charleshenri.guerin@me.com>
2018-10-31 16:08:44 +0100

    :recycle: Run apt or yum if available but not both (#52)
    * check if apt or yum is available
    * use selected package manager

--- user_data.sh
+++ user_data.sh
@@ -3,16 +3,20 @@
 ##############
 # Install deps
 ##############
-# Ubuntu
-apt-get update
-apt-get install python-pip jq -y
-#####################
 
-# Amazon Linux (RHEL) - NAT instances
-yum update -y
-# epel provides python-pip & jq
-yum install -y epel-release
-yum install python-pip jq -y
+# Apt based distro
+if command -v apt-get &>/dev/null; then
+  apt-get update
+  apt-get install python-pip jq -y
+
+# Yum based distro
+elif command -v yum &>/dev/null; then
+  yum update -y
+  # epel provides python-pip & jq
+  yum install -y epel-release
+  yum install python-pip jq -y
+fi
+
 #####################
 
 pip install --upgrade awscli
