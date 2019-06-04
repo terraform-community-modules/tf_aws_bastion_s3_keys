8e5d8c186aa8f73db355ff5cd8a54c579a0d596b
Elias Dorneles <eliasdorneles@gmail.com>
2019-06-04 18:02:28 +0200

    0.12 upgrade: remove list brackets which create another list

--- main.tf
+++ main.tf
@@ -125,31 +125,21 @@ resource "aws_autoscaling_group" "bastion" {
     "GroupTotalInstances",
   ]
 
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibilty in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
-  tags = [
-    concat(
-      [
-        {
-          "key"                 = "Name"
-          "value"               = var.name
-          "propagate_at_launch" = true
-        },
-        {
-          "key"                 = "EIP"
-          "value"               = var.eip
-          "propagate_at_launch" = true
-        },
-      ],
-      var.extra_tags,
-    ),
-  ]
+  tags = concat(
+    [
+      {
+        "key"                 = "Name"
+        "value"               = var.name
+        "propagate_at_launch" = true
+      },
+      {
+        "key"                 = "EIP"
+        "value"               = var.eip
+        "propagate_at_launch" = true
+      },
+    ],
+    var.extra_tags,
+  )
 
   lifecycle {
     create_before_destroy = true
